d237e2aaa8aa469b58717f9df90774810e514fcb
maycon-mello <maycon.mello@objectedge.com>
2016-08-31 15:01:17 -0300

    creating project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = new express();
+
+app.use(express.static('public'));
+
+app.get('/ShoppingCart/index.cfm',
+  (req, res) => res.sendFile('plexus_review.html', { root: './pages' })
+);
+
+app.listen(80, () => console.log('Server running on port 9090'));
