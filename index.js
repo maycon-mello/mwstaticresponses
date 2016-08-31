f95d5c7e109e79c7941bfc9a26a30818e067bd12
Xiaoping <xiaoping.li@moovweb.com>
2016-08-31 12:17:35 -0700

    test page

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/ShoppingCart/index.cfm',
   (req, res) => res.sendFile('plexus_review.html', { root: './pages' })
 );
 
+app.get('/test',
+  (req, res) => res.sendFile('preferred_confirmation.html', { root: './pages' })
+);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
