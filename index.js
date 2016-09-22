ced5be93d1b3aedb0a9ab016b3cfba9f4adbe926
Xiaoping <xiaoping.li@moovweb.com>
2016-09-22 12:30:47 -0700

    changed to another confirmation page

--- index.js
+++ index.js
@@ -7,12 +7,8 @@ app.get('/', (req, res) => res.end('Server is running'));
 
 
 app.get('/ShoppingCart/index.cfm',
-  (req, res) => res.sendFile('ambassador_confirmation.html', { root: './pages' })
+  (req, res) => res.sendFile('loggedIn_confirmation.html', { root: './pages' })
 );
-
-// app.get('/ShoppingCart/index.cfm',
-//   (req, res) => res.sendFile('loggedIn_confirmation.html', { root: './pages' })
-// );
 
 // app.get('/ShoppingCart/index.cfm',
 //   (req, res) => res.sendFile('retail_confirmation.html', { root: './pages' })
@@ -22,8 +18,12 @@ app.get('/ShoppingCart/index.cfm',
 //   (req, res) => res.sendFile('plexus_review.html', { root: './pages' })
 // );
 
+// app.get('/Application/index.cfm',
+//   (req, res) => res.sendFile('preferred_confirmation.html', { root: './pages' })
+// );
+
 app.get('/Application/index.cfm',
-  (req, res) => res.sendFile('preferred_confirmation.html', { root: './pages' })
+  (req, res) => res.sendFile('ambassador_confirmation.html', { root: './pages' })
 );
 
 const port = process.env.PORT || 3000;
